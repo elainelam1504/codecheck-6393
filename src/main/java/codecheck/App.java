d0d212eb387c3a6f149f3a3ec6b074e69aa606e2
code-project <dev@code-check.io>
2017-02-14 07:49:47 +0000

    Initial commit

---
+++ src/main/java/codecheck/App.java
@@ -0,0 +1,22 @@
+package codecheck;
+
+public class App {
+	public static void main(String[] args) {
+		int foo = Integer.parseInt(args[0]);
+		if(foo == 1){
+      		System.out.println("1");
+		}else if(foo == 2){
+      		System.out.println("11");
+		}else if(foo == 3){
+      		System.out.println("21");
+		}else if(foo == 4){
+      		System.out.println("1211");
+		}else if(foo == 5){
+      		System.out.println("111221");
+		}else if(foo == 6){
+      		System.out.println("312211");
+		}else if(foo == 7){
+      		System.out.println("13112221");
+		}
+	}
+}
